dfb52354297d6cb8726676dbf5fb6c034b449c48
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-05-09 00:33:37 +0900

    [Back-Add] auth router 생성을 위한 passport 모듈 세팅

---
+++ simplesns-backend/passport/index.js
@@ -0,0 +1,19 @@
+const passport = require("passport");
+const User = require("../models/user");
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(user);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      //유저 모델의 id nick을 다 가져온다
+      //user 모델의 associate 메서드 참고
+      include: [{ model: User, attributes: ["id", "nick"], as: "Followers" }],
+    })
+      .then((user) => done(null, user))
+      .catch((error) => done(error));
+  });
+};
