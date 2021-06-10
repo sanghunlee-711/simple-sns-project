6f04125e73dabb19ac97e44a27dbb0d5fdeac725
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-06-10 23:58:09 +0900

    [Front-Add] User > 팔로우 API 와 연결

--- simplesns-frontend/src/pages/User/User.tsx
+++ simplesns-frontend/src/pages/User/User.tsx
@@ -1,12 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import styled from "styled-components";
-import {
-  getFollowData,
-  sendFollow,
-  sendUnFollow,
-} from "../../redux/reducer/followReducer";
+import { getFollowData, sendFollow } from "../../redux/reducer/followReducer";
 import { RootState } from "../../redux/store";
 import UserCard from "./components/UserCard";
 interface FollowData {
@@ -22,6 +18,7 @@ interface FollowList {
 
 export default function User() {
   const { userId } = useParams<{ userId: string }>();
+  const history = useHistory();
   const userData: FollowList = useSelector(
     (state: RootState) => state.followReducer.followData
   );
@@ -34,21 +31,13 @@ export default function User() {
       dispatch(getFollowData(userId));
     }
 
-    if (sessionStorage.getItem("userId") !== userId) {
+    if (sessionStorage.getItem("userId") === undefined) {
+      return history.push("/");
     }
   }, []);
 
-  const handleFollow = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const { name, value } = e.target as HTMLButtonElement;
-    if (name === "follow") {
-      dispatch(sendFollow(Number(value)));
-    }
-
-    if (name === "unFollow") {
-      dispatch(sendUnFollow(Number(value)));
-    }
-
-    console.log("follow Check");
+  const handleFollow = () => {
+    dispatch(sendFollow(Number(userId)));
   };
 
   return (
@@ -58,6 +47,21 @@ export default function User() {
       }}
     >
       <HomeWrapper>
+        {Number(sessionStorage.getItem("userId")) !== Number(userId) &&
+        userId !== "my" ? (
+          <FollowButtonWrapper>
+            <button
+              onClick={() => {
+                handleFollow();
+              }}
+            >
+              얘를 팔로우 하자
+            </button>
+          </FollowButtonWrapper>
+        ) : (
+          ""
+        )}
+
         <FollowersContainer>
           {userData?.Followers.length <= 0 ? (
             <div>나를 아무도 팔로우 하지 않네</div>
@@ -90,6 +94,8 @@ export default function User() {
     </HomeContainer>
   );
 }
+
+const FollowButtonWrapper = styled.div``;
 
 const HomeContainer = styled.main`
   margin: 90px auto 0px auto;
